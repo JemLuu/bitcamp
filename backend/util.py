56247bd474b9614365013f96b8771a91cd0e3e53
Rachel J <rcj.rachel@gmail.com>
2025-04-11 22:05:01 -0400

    cleaned up

--- backend/util.py
+++ backend/util.py
@@ -8,31 +8,33 @@ def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode("utf-8")
 
-
-# Path to your image
-image_path = "../ducks.jpeg"
-
-# Getting the Base64 string
-base64_image = encode_image(image_path)
-
-
-response = client.responses.create(
-    model="gpt-4-turbo",
-    input=[
-        {
-            "role": "user",
-            "content": [
-                { "type": "input_text", "text": "how would you describe what you are seeing to a blind person? limit your response to two sentences. start with 'there is...' " },
-                {
-                    "type": "input_image",
-                    "image_url": f"data:image/jpeg;base64,{base64_image}",
-                },
-            ],
-        }
-    ],
-)
-
-print(response.output_text)
-
+# send image to ChatGPT and return descriptor text
 def process_image():
-    image = "ducks.avif"
+    # TODO: get from frontend
+    image_path = "../ducks.jpeg"
+
+    base64_image = encode_image(image_path)
+
+    response = client.responses.create(
+        model="gpt-4o",
+        input=[
+            {
+                "role": "user",
+                "content": [
+                    { "type": "input_text", "text": "how would you describe what you are seeing to a blind person? limit your response to two sentences. start with 'there is...' " },
+                    {
+                        "type": "input_image",
+                        "image_url": f"data:image/jpeg;base64,{base64_image}",
+                    },
+                ],
+            }
+        ],
+    )
+    response.output_text
+
+# send descriptor text to TTS api
+def tts():
+    # TODO: 
+    return None
+
+process_image()
